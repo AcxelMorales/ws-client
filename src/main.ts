9d431901a864dd3579d1607099c94734bb5aa19d
Acxel Morales <acxelmorales97@gmail.com>
2023-06-06 17:51:15 -0600

    Se agregaron conexiones y desconexiones de los sockets

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css';
+
+import { connectToServer } from './socket-client.ts';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Websocket - Client</h1>
+    <span id="server-status">Offline</span>
+  </div>
+`;
+
+connectToServer();
